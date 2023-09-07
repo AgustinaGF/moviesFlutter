3803f2f6b199b9f3f60403d0272719c742e5474e
AgustinaGF <agustinagomez385@gmail.com>
2023-09-07 18:11:10 -0300

    has internet

--- lib/app/presentation/modules/splash/views/splash_view.dart
+++ lib/app/presentation/modules/splash/views/splash_view.dart
@@ -1,10 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:movies_flutter/main.dart';
 
-class SplashView extends StatelessWidget {
+class SplashView extends StatefulWidget {
   const SplashView({super.key});
 
   @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPersistentFrameCallback(
+      (_) {
+        _init();
+      },
+    );
+  }
+
+  Future<void> _init() async {
+    final injector = Injector.of(context);
+    final connectivityRepository = injector.connectivityRepository;
+    final hasInternet = await connectivityRepository.hasInternet;
+
+    if (hasInternet) {
+      final authenticationRepository = injector.authenticationRepository;
+      final isSigned = await authenticationRepository.isSignedInl;
+      if (isSigned) {}
+    } else {}
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Scaffold(
+      body: Center(
+          child: SizedBox(
+              width: 80, height: 80, child: CircularProgressIndicator())),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,36 @@
 import 'package:flutter/widgets.dart';
+import 'package:movies_flutter/app/data/repositories_implementation/authentication_repository_impl.dart';
+import 'package:movies_flutter/app/data/repositories_implementation/connectivity_repository_imp.dart';
+import 'package:movies_flutter/app/domain/repositories/authentication_repository.dart';
+import 'package:movies_flutter/app/domain/repositories/connectivity_repository.dart';
 import 'package:movies_flutter/app/my_app.dart';
 
 void main() {
   runApp(
-    Myapp(),
+    Injector(
+      connectivityRepository: ConnectivityRepositoryImp(),
+      authenticationRepository: AuthenticationRepositoryImpl(),
+      child: const Myapp(),
+    ),
   );
+}
+
+class Injector extends InheritedWidget {
+  const Injector({
+    super.key,
+    required super.child,
+    required this.connectivityRepository,
+    required this.authenticationRepository,
+  });
+
+  final ConnectivityRepository connectivityRepository;
+  final AuthenticationRepository authenticationRepository;
+
+  @override
+  bool updateShouldNotify(_) => false;
+  static Injector of(BuildContext context) {
+    final injector = context.dependOnInheritedWidgetOfExactType<Injector>();
+    assert(injector != null, 'Injector could not be found');
+    return injector!;
+  }
 }
