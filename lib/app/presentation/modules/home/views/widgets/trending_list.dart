a8ed1605b790f5623b24ab1a94af5da04a5af7d4
AgustinaGF <agustinagomez385@gmail.com>
2023-11-29 19:03:54 -0300

    add images

--- lib/app/presentation/modules/home/views/widgets/trending_list.dart
+++ lib/app/presentation/modules/home/views/widgets/trending_list.dart
@@ -4,31 +4,55 @@ import 'package:movies_flutter/app/domain/enums.dart';
 import 'package:movies_flutter/app/domain/failures/http_request_failure/http_request_failure.dart';
 import 'package:movies_flutter/app/domain/models/media/media.dart';
 import 'package:movies_flutter/app/domain/repositories/trending_repository.dart';
+import 'package:movies_flutter/app/presentation/global/utils/get_image_url.dart';
 import 'package:provider/provider.dart';
 
 typedef EitherListMedia = Either<HttpRequestFailure, List<Media>>;
 
-class TrendingList extends StatelessWidget {
+class TrendingList extends StatefulWidget {
   const TrendingList({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final TrendingRepository repository = context.read();
+  State<TrendingList> createState() => _TrendingListState();
+}
 
+class _TrendingListState extends State<TrendingList> {
+  late final Future<EitherListMedia> _future;
+
+  @override
+  void initState() {
+    super.initState();
+    final TrendingRepository repository = context.read();
+    _future = repository.getMoviesAndSeries(TimeWindow.day);
+  }
+
+  @override
+  Widget build(BuildContext context) {
     return SizedBox(
       height: 250,
       child: Center(
         child: FutureBuilder<EitherListMedia>(
-            future: repository.getMoviesAndSeries(TimeWindow.day),
+            future: _future,
             builder: (_, snapshot) {
-              if (snapshot.connectionState == ConnectionState.waiting) {
+              if (!snapshot.hasData) {
                 return CircularProgressIndicator();
               }
-              if (snapshot.hasError) {
-                return const Text('Error');
-              }
-              return Text(
-                snapshot.data?.toString() ?? 'empty data',
+              return snapshot.data!.when(
+                left: (failure) => Text(
+                  failure.toString(),
+                ),
+                right: (list) {
+                  return ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemBuilder: (_, index) {
+                      final media = list[index];
+                      return Image.network(
+                        getImageUrl(media.posterPath),
+                      );
+                    },
+                    itemCount: list.length,
+                  );
+                },
               );
             }),
       ),
