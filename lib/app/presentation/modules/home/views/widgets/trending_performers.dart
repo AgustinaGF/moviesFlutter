4928f66a539d8a01696ab75cc7e288b3dd6590c6
AgustinaGF <agustinagomez385@gmail.com>
2024-01-13 20:04:47 -0300

    some changes in performers

--- lib/app/presentation/modules/home/views/widgets/trending_performers.dart
+++ lib/app/presentation/modules/home/views/widgets/trending_performers.dart
@@ -1,8 +1,10 @@
+import 'package:extended_image/extended_image.dart';
 import 'package:flutter/material.dart';
 import 'package:movies_flutter/app/domain/either/either.dart';
 import 'package:movies_flutter/app/domain/failures/http_request_failure/http_request_failure.dart';
 import 'package:movies_flutter/app/domain/models/performer/performer.dart';
 import 'package:movies_flutter/app/domain/repositories/trending_repository.dart';
+import 'package:movies_flutter/app/presentation/global/utils/get_image_url.dart';
 import 'package:provider/provider.dart';
 
 typedef EitherListPerformer = Either<HttpRequestFailure, List<Performer>>;
@@ -25,19 +27,45 @@ class _TrendingPerformersState extends State<TrendingPerformers> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<EitherListPerformer>(
-      future: _future,
-      builder: (_, snapshop) {
-        if (!snapshop.hasData) {
-          return const Center(
-            child: CircularProgressIndicator(),
+    final width = MediaQuery.of(context).size.width;
+    return Expanded(
+      child: FutureBuilder<EitherListPerformer>(
+        future: _future,
+        builder: (_, snapshop) {
+          if (!snapshop.hasData) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+          return snapshop.data!.when(
+            left: (_) => const Text('Error'),
+            right: (list) => ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                final performer = list[index];
+                return SizedBox(
+                  width: width,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(10),
+                      child: Stack(
+                        children: [
+                          Positioned.fill(
+                            child: ExtendedImage.network(
+                                getImageUrl(performer.profilePath)),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+              itemCount: list.length,
+            ),
           );
-        }
-        return snapshop.data!.when(
-          left: (_) => Text('Error'),
-          right: (list) => Text('Performers'),
-        );
-      },
+        },
+      ),
     );
   }
 }
